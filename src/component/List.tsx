cb76e26f0f7920ebfb33c024e19eb0121a6bdf9a
Ahmed Said <axmedsaid098@gmail.com>
2026-08-16 14:05:22 +0300

    fixed list loading

--- src/component/List.tsx
+++ src/component/List.tsx
@@ -1,37 +1,58 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { MessageSquare, MoreHorizontal } from "lucide-react";
+import { AlertCircle, Loader2, MessageSquare, MoreHorizontal } from "lucide-react";
 import Fetch from "../api/fetch";
 
 export default function List({ onNavigate }: { onNavigate?: () => void }) {
   const [notes, setNotes] = useState([]);
   const navigate = useNavigate();
-
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchNotes = async () => {
+      setLoading(true);
       try {
         const res = await Fetch("tasks");
         setNotes(res);
-      } catch (err) {
-        if(err === 'Invalid token'){
+      } catch (err: any) {
+        setError(err);
+        if (err === 'Invalid token') {
           window.location.href = '/login'
           console.log('Invalid token. Please login again.');
           return;
         }
         console.error("Error fetching notes:", err);
       }
+      finally {
+        setLoading(false)
+      }
     };
     fetchNotes();
   }, []);
-
+  {
+    error && (
+      <div className="mb-6 p-4 bg-red-500/10 border border-red-500/20 rounded-2xl flex items-start gap-3 text-red-400 animate-in">
+        <AlertCircle size={20} className="flex-shrink-0" />
+        <p className="text-sm font-medium">{error}</p>
+      </div>
+    )
+  }
+  
+    if (loading) {
+      return (
+        <div className="h-[80vh] flex flex-col items-center justify-center space-y-4">
+          <Loader2 className="animate-spin text-indigo-500" size={32} />
+        </div>
+      );
+    }
   return (
     <ul className="space-y-1">
       {notes.map((note: any) => (
         <li key={note._id}>
-          <button 
-            onClick={() => { 
-                navigate(`/note/${note._id}`); 
-                if(onNavigate) onNavigate(); // Xir Sidebar-ka mobile-ka
+          <button
+            onClick={() => {
+              navigate(`/note/${note._id}`);
+              if (onNavigate) onNavigate(); // Xir Sidebar-ka mobile-ka
             }}
             className="flex items-center justify-between w-full p-3 rounded-xl hover:bg-[#1a1b1e] transition-all group text-left border border-transparent hover:border-white/5"
           >
